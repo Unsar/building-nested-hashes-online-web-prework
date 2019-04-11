bc47936ccf6a54f220c1a18f63d85dde10acfad1
Unsar Malik <unsar.malik24@gmail.com>
2019-04-11 23:46:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      (:patriarch => "Lord Montague", :matriarch => "Lady Montague", :hero => "Romeo", :hero_friends => "Benvolvio", "Mercutio"])}, 
+      (:patriarch => "Lord Montague", :matriarch => "Lady Montague", :hero => "Romeo", :hero_friends => "Benvolvio", "Mercutio")}, 
     
-    :capulet => {([:patriarch => "Lord Capulet", :matriarch => "Lady Capulet", :heroine => "Juliet", :heroine_friends => "Steven", "Nurse"])}}
+    :capulet => {
+      (:patriarch => "Lord Capulet", :matriarch => "Lady Capulet", :heroine => "Juliet", :heroine_friends => "Steven", "Nurse")}}
 end
